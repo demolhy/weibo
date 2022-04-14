2d55755faa0fa0e8350de8d872f9e4b19d130db1
linhaiyu <linhaiyu@rongtaipharm.com>
2022-04-14 18:16:11 +0800

    change route problem

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 // import { renderRoutes } from "react-router-config";
 // import routes from "./routes/index.js";
 import Home from './layouts/HomeLayouts'
+import Index from './pages/Index'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './style/index.scss'
 
@@ -9,9 +10,10 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/index' element={<Home />}></Route>
-        <Route path='/' element={<Home />}></Route>
+        <Route path='/index' element={<Index />}></Route>
+        <Route path='/' element={<Index />}></Route>
       </Routes>
+      <Home></Home>
     </BrowserRouter>
   )
 }
--- src/layouts/HomeLayouts.js
+++ src/layouts/HomeLayouts.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 // import { renderRoutes } from "react-router-config";
 
 function Home(props) {
@@ -12,22 +13,30 @@ function Home(props) {
         {renderRoutes(route.routes)}
       </div> */}
       <div className='footer_fixed'>
-        <div className='nav'>
-          <i className='iconfont icon-homepage_fill'></i>
-          <div>首页</div>
-        </div>
-        <div className='nav'>
-          <i className='iconfont icon-search'></i>
-          <div>热搜</div>
-        </div>
-        <div className='nav'>
-          <i className='iconfont icon-message'></i>
-          <div>消息</div>
-        </div>
-        <div className='nav'>
-          <i className='iconfont icon-mine'></i>
-          <div>我</div>
-        </div>
+        <Link to='/index'>
+          <div className='nav'>
+            <i className='iconfont icon-homepage_fill'></i>
+            <div>首页</div>
+          </div>
+        </Link>
+        <Link to='/index1'>
+          <div className='nav'>
+            <i className='iconfont icon-search'></i>
+            <div>热搜</div>
+          </div>
+        </Link>
+        <Link to='/index2'>
+          <div className='nav'>
+            <i className='iconfont icon-message'></i>
+            <div>消息</div>
+          </div>
+        </Link>
+        <Link to='/index3'>
+          <div className='nav'>
+            <i className='iconfont icon-mine'></i>
+            <div>我</div>
+          </div>
+        </Link>
       </div>
     </div>
   )
