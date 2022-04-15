13f6a4ec5322510e61c49addd34de084d9d6cd76
linhaiyu <linhaiyu@rongtaipharm.com>
2022-04-15 18:19:45 +0800

    添加swiper插件

--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+
 
 function Index() {
   return (
-    <div>123</div>
+    <Swiper
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
   )
 }
 
